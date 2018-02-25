bc9c6f077c740e744a183b5bde51a83ec35cecd7
Chris Ha <chunghha@users.noreply.github.com>
2018-02-24 21:49:02 -0600

    fetch data with axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,19 +5,19 @@
 </template>
 <template>
   <el-table
-    :data="tableData3"
+    :data="users"
     style="width: 100%">
     <el-table-column type="expand">
       <template slot-scope="props">
-        <p>State: {{ props.row.state }}</p>
-        <p>City: {{ props.row.city }}</p>
-        <p>Address: {{ props.row.address }}</p>
-        <p>Zip: {{ props.row.zip }}</p>
+        <p>Street: {{ props.row.address.street }}</p>
+        <p>Suite: {{ props.row.address.suite }}</p>
+        <p>City: {{ props.row.address.city }}</p>
+        <p>Zip: {{ props.row.address.zipcode }}</p>
       </template>
     </el-table-column>
     <el-table-column
-      label="Date"
-      prop="date">
+      label="Email"
+      prop="email">
     </el-table-column>
     <el-table-column
       label="Name"
@@ -27,61 +27,25 @@
 </template>
 
 <script>
-  export default {
-    name: 'about',
-    data() {
-      return {
-        tableData3: [{
-          date: '2016-05-03',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-02',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-04',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-01',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-08',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-06',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-07',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }]
-      }
+import axios from 'axios';
+
+export default {
+  name: 'about',
+  data() {
+    return {
+      users: [],
+      errors: []
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get(
+        `http://jsonplaceholder.typicode.com/users`
+      );
+      this.users = response.data;
+    } catch (e) {
+      this.errors.push(e);
     }
   }
+};
 </script>
