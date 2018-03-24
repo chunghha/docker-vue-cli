cfda43e8abc2a6e24800bde93b12243f66b15bfa
Chris Ha <chunghha@users.noreply.github.com>
2018-03-24 01:25:51 -0500

    chore: video control buttons

--- src/views/Youtube.vue
+++ src/views/Youtube.vue
@@ -3,10 +3,14 @@
     <v-flex xs12 sm6 offset-sm3>
       <v-card>
         <v-card-media>
-          <youtube :video-id="videoId" ref="youtube" @playing="playing"></youtube>
+          <youtube :video-id="videoId" :player-vars="playerVars"
+            ref="youtube" @playing="playing"></youtube>
         </v-card-media>
         <v-card-actions>
-          <v-btn @click="playVideo">Play</v-btn>
+          <v-btn :disabled="isPlaying" @click="playVideo"
+            color="teal darken-4" block>Play</v-btn>
+          <v-btn :disabled="!isPlaying" @click="stopVideo"
+            color="deep-orange darken-4" block>Stop</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -17,15 +21,27 @@
 export default {
   data() {
     return {
-      videoId: 'lG0Ys-2d4MA'
+      videoId: 'lG0Ys-2d4MA',
+      playerVars: {
+        showInfo: 0
+      },
+      isPlaying: false
     };
   },
   methods: {
     playVideo() {
       this.player.playVideo();
+      this.isPlaying = true;
     },
     playing() {
       console.log('o/ we are watching!!!');
+      this.isPlaying = true;
+    },
+    stopVideo() {
+      this.player.stopVideo().then(() => {
+        console.log('It is stopped!');
+        this.isPlaying = false;
+      });
     }
   },
   computed: {
