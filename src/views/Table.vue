4131fe26743ab50100b6f5f3a36ff9a1b89d9ff9
Chris Ha <chunghha@users.noreply.github.com>
2018-03-24 09:54:52 -0500

    chore: more slot on v-data-table

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,23 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
-<template>
-  <v-data-table
-    :headers="headers"
-    :items="users"
-    class="elevation-4"
-  >
-    <template slot="items" slot-scope="props">
-      <td>{{ props.item.id }}</td>
-      <td>{{ props.item.name }}</td>
-      <td>{{ props.item.email }}</td>
-      <td>{{ props.item.company.name }}</td>
-      <td>{{ props.item.phone }}</td>
-      <td>{{ props.item.website }}</td>
-  </template>
-  </v-data-table>
+  <v-card>
+    <v-card-title>
+      Contacts
+      <v-spacer></v-spacer>
+      <v-text-field
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+        v-model="search"
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="users"
+      :loading="loading"
+      :search="search"
+      v-model="selected"
+      item-key="name"
+      select-all
+      class="elevation-4"
+    >
+    <v-progress-linear slot="progress" color="deep-orange darken4" indeterminate></v-progress-linear>
+      <template slot="items" slot-scope="props">
+        <td>
+          <v-checkbox
+            primary
+            hide-details
+            v-model="props.selected"
+          ></v-checkbox>
+        </td>
+        <td>{{ props.item.id }}</td>
+        <td>{{ props.item.name }}</td>
+        <td>{{ props.item.email }}</td>
+        <td>{{ props.item.company.name }}</td>
+        <td>{{ props.item.phone }}</td>
+        <td>{{ props.item.website }}</td>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -27,20 +48,19 @@ export default {
   name: 'about',
   data() {
     return {
+      search: '',
+      selected: [],
       headers: [
-        {
-          sortable: false,
-          text: 'ID',
-          value: 'id'
-        },
-        { text: 'Name', value: 'name' },
-        { text: 'Email', value: 'email' },
-        { text: 'Company', value: 'company.name' },
-        { text: 'Phone', value: 'phone' },
-        { text: 'Website', value: 'website' }
+        { text: 'ID', value: 'id', align: 'center' },
+        { text: 'Name', value: 'name', align: 'center' },
+        { text: 'Email', value: 'email', align: 'center' },
+        { text: 'Company', value: 'company.name', align: 'center' },
+        { text: 'Phone', value: 'phone', align: 'center' },
+        { text: 'Website', value: 'website', align: 'center' }
       ],
       users: [],
-      errors: []
+      errors: [],
+      loading: true
     };
   },
   async created() {
@@ -49,6 +69,10 @@ export default {
         `http://jsonplaceholder.typicode.com/users`
       );
       this.users = response.data;
+      // fake delay
+      const delay = ms => new Promise(res => setTimeout(() => res(), ms));
+      await delay(1000);
+      this.loading = false;
     } catch (e) {
       this.errors.push(e);
     }
