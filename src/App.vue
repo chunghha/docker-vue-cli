51433024ce7fa3b70c2f10eee35e079ee2d550aa
Chris Ha <chunghha@users.noreply.github.com>
2018-03-24 18:56:13 -0500

    chore: Menu toggle

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer app class="blue-gray darken-4" dark
-      stateless hide-overlay :mini-variant.sync="mini" v-model="drawer">
-      <v-toolbar flat class="transparent">
-        <v-list>
-          <v-list-tile avata>
-            <v-list-tile-avatar>
-              <img src="./assets/logo.png" >
-            </v-list-tile-avatar>
-            <v-list-tile-action>
-              <v-btn icon @click.native.stop="mini = !mini">
-                <v-icon>chevron_left</v-icon>
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      :mini-variant="mini"
+      dark
+      absolute
+    >
+      <v-list class="pa-1">
+        <v-list-tile v-if="mini" @click.stop="mini = !mini">
+          <v-list-tile-action>
+            <v-icon>chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="./assets/logo.png" >
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Docker Vue CLI</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon @click.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
             </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-      <v-divider></v-divider>
-      <v-list dense class="pt-0">
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider light></v-divider>
         <v-list-tile v-for="item in items" :key="item.title" @click="">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
@@ -42,6 +53,9 @@
         <router-link to="/tabs">Tabs</router-link> |
         <router-link to="/youtube">Youtube</router-link>
       </div>
+      <v-flex align-end>
+        <v-btn @click.stop="drawer = !drawer" color="primary" dark>Menu</v-btn>
+      </v-flex>
     </v-toolbar>
     <v-content>
       <v-container fluid>
@@ -65,7 +79,7 @@
         ],
         right: null,
         drawer: true,
-        mini: true,
+        mini: false,
       }
     }
   }
